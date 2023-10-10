a54f3e51399d57cc60420473e4fc4c72962a4204
ed <edward@commodum.io>
2023-10-10 04:41:06 +0000

    proof of concept completed

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	// "log"
 	"os"
 	"strconv"
@@ -16,10 +17,13 @@ type Config struct {
 
 func parseFlags() *Config {
 	flag.Parse()
+	checkDefaults()
 	if chainId = getFlag(chainId, os.Getenv("TMD_CHAIN_ID")); len(chainId) <= 0 {
+		fmt.Printf("Using default chain-id: %v\n", defaultChainId)
 		chainId = defaultChainId
 	}
 	if initialRpcs = getFlag(initialRpcs, os.Getenv("TMD_INITIAL_RPCS")); len(initialRpcs) <= 0 {
+		fmt.Printf("Using default initial RPCs: %v\n", defaultInitialRpcs)
 		initialRpcs = defaultInitialRpcs
 	}
 	if !inferChainId {
@@ -46,4 +50,28 @@ func getFlag(flag, env string) string {
 func getenvBool(env string) bool {
 	val, _ := strconv.ParseBool(env)
 	return val
+}
+
+func checkDefaults() {
+	flags := map[string]*flag.Flag{}
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Name == "infer-chain-id" || f.Name == "state-sync" {
+			return
+		}
+		flags[f.Name] = f
+	})
+
+	isSet := map[string]bool{"chain-id": false, "initial-rpcs": false}
+	for name, _ := range isSet {
+		flag.Visit(func(f *flag.Flag) {
+			if f.Name == name {
+				isSet[name] = true
+			}
+		})
+	}
+	for name, flag := range flags {
+		if !isSet[name] {
+			fmt.Printf("%v flag not set, using default %v\n", flag.Name, flag.Value)
+		}
+	}
 }
